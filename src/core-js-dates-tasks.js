467264522d4c98a2cdcf53d8aa79f7495613ee54
Nikita8Sannikov <Ak-4t7@mail.ru>
2025-03-24 02:13:23 +0600

    fix: fix getTime

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -32,7 +32,10 @@ function dateToTimestamp(date) {
  * Date(2015, 10, 20, 23, 15, 1) => '23:15:01'
  */
 function getTime(date) {
-  return new Date(date).toLocaleTimeString();
+  return new Date(date).toLocaleTimeString('en-US', {
+    timeZone: 'UTC',
+    hour12: false,
+  });
 }
 
 /**
